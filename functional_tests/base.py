efe0241263c8ec4bf5a65f710614dfe870c3bbd2
Paul Dejean <paul@Pauls-MacBook-Pro.local>
2019-01-19 17:47:40 +0100

    First cut FT for item validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,11 +7,12 @@ import os
 
 MAX_WAIT = 5
 
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server :
+        if staging_server:
             self.live_server_url = "http://" + staging_server
 
     def tearDown(self):
@@ -30,4 +31,12 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
