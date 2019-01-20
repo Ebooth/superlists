a829340d9f5eee9373ba9b283ea0e48ba6739e3d
Paul Dejean <paul@Pauls-MacBook-Pro.local>
2019-01-21 00:27:14 +0100

    add jquery, qunit tests, list.js with keypress listeners

---
+++ lists/static/list.js
@@ -0,0 +1,11 @@
+$('input[name="text"]').on('keypress', function () {
+    $('.has-error').hide();
+});
+
+var initialize = function () {
+    $('input[name="text"]').on('keypress', function () {
+        $('.has-error').hide();
+    });
+};
+
+
