851fb967bfa7eee9d0f9b421675fa01023aecc51
Paul Dejean <paul@Pauls-MacBook-Pro.local>
2019-01-23 16:06:28 +0100

    Corrected Logger

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,7 +139,6 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'level': 'DEBUG',
             'formatter' : 'simple',
-            'stream': sys.stdout
         },
     },
     'loggers': {
